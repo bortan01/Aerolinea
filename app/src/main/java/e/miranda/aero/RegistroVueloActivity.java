29338cf8e022bbc9ce22f9c98efa1e527822076d
Boris Ricardo Miranda <brmiranda009@gmail.com>
2019-07-02 00:43:07 -0600

    ya guarda vuelo

--- app/src/main/java/e/miranda/aero/RegistroVueloActivity.java
+++ app/src/main/java/e/miranda/aero/RegistroVueloActivity.java
@@ -1,8 +1,11 @@
 package e.miranda.aero;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +16,8 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+import com.loopj.android.http.RequestParams;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -27,6 +32,9 @@ public class RegistroVueloActivity extends AppCompatActivity {
     Button despeje, aterrisaje;
     int idDespeje;
     int idAterrisaje;
+    RequestParams parametros;
+
+    TextInputLayout  primera, ejecutiva,economica;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,10 +44,16 @@ public class RegistroVueloActivity extends AppCompatActivity {
         txtAterrisaje = findViewById(R.id.txtAterrisaje);
         txtDespeje = findViewById(R.id.txtDespeje);
         despeje = findViewById(R.id.despeje);
+        primera = findViewById(R.id.primera);
+        ejecutiva = findViewById(R.id.ejecutivo);
+        economica = findViewById(R.id.economicos);
         aterrisaje = findViewById(R.id.aterrisaje);
         hora = findViewById(R.id.hora);
         comboAvion = findViewById(R.id.comboAvion);
         llenarComboAvion();
+        idAterrisaje = 0;
+        idDespeje = 0 ;
+        parametros = new RequestParams();
 
         fecha.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,11 +99,7 @@ public class RegistroVueloActivity extends AppCompatActivity {
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
-    public void guardarVuelo(View view) {
-    }
 
-    public void Cancelar(View view) {
-    }
 
     public  void llenarComboAvion(){
         String[] opciones = {"Mayuscula", "Minuscula"};
@@ -119,5 +129,75 @@ public class RegistroVueloActivity extends AppCompatActivity {
             }
         }
 
+    }
+    public void guardarVuelo(View view) {
+        Avion avion = (Avion) comboAvion.getSelectedItem();
+        int idAvion =  avion.getId_avion();
+
+        if(fecha.getText().toString().equals("")){
+            msg("ingrese la fecha");
+        }else if(hora.getText().toString().equals("")){
+            msg("ingrese la hora");
+        }else if(idAvion == 0){
+            msg("Seleccione el avion");
+        }else if(txtDespeje.getText().toString().equals("")){
+            msg("seleccione el aeropuerto de despeje");
+        }else if(txtAterrisaje.getText().toString().equals("")){
+            msg("seleccione el aeropuerto de Aterrisaje");
+        }else if(economica.getEditText().getText().toString().equals("")){
+            msg("ingrese el precio de la clase economica");
+
+        }else if(ejecutiva.getEditText().getText().toString().equals("")){
+            msg("ingrese el precio de la clase Ejecutiva");
+
+        }else if(primera.getEditText().getText().toString().equals("")){
+            msg("ingrese el precio de la Primera Clase");
+
+        }else {
+
+
+            parametros.put("origen" , idDespeje);
+            parametros.put("destino",idAterrisaje);
+            parametros.put("id_avion",idAvion);
+            parametros.put("fecha",fecha.getText().toString() +" " + hora.getText().toString());
+            parametros.put("economica",economica.getEditText().getText().toString());
+            parametros.put("ejecutiva",ejecutiva.getEditText().getText().toString());
+            parametros.put("primera",primera.getEditText().getText().toString());
+            parametros.put("accion", "guardarVuelo");
+            conexion.escribir(parametros);
+
+            limpiar();
+        }
+
+    }
+
+    private void limpiar() {
+        hora.setText("");
+        fecha.setText("");
+        txtAterrisaje.setText("");
+        txtDespeje.setText("");
+        economica.getEditText().setText("");
+        ejecutiva.getEditText().setText("");
+        primera.getEditText().setText("");
+    }
+
+
+    public void Cancelar(View view) {
+    }
+
+    private void msg(String texto) {
+        AlertDialog.Builder dialogo = new AlertDialog.Builder(this);
+        dialogo.setTitle("INFORMACION");
+        dialogo.setMessage(texto);
+
+
+        dialogo.setPositiveButton("aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ////aqui se pone la accion si da click en aceptar
+            }
+        });
+
+        dialogo.show();
     }
 }
